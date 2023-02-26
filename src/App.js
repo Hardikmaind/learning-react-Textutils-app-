1041bb2fb99d34f06045644cf2239980f805443a
Hardikmaind <maindhardik@gmail.com>
2023-02-26 18:53:28 +0530

    vid-16 harry react course

--- src/App.js
+++ src/App.js
@@ -1,59 +1,53 @@
+/* eslint-disable no-unused-vars */
 
-import './App.css';
-// import About from './Components/About';
-import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
+import "./App.css";
+import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
 // imrs keyword short cut for below
-import { useState } from 'react';
-import Alert from './Components/Alert';
+import { useState } from "react";
+import Alert from "./Components/Alert";
+// since we have imported Browser Router as Router then we have to enclose all the <Routes/> in <Router> </Router>. if we directly import BrowserRouter then we have to enclose <Routes/> in <BrowserRouter> </BrowserRouter>
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   // i want app.js to have the control
 
-
-    
-    //state for alert
-    const [alert, setAlert] = useState(null);
-    // here we will be  defining alert/setAlert as an object and below i will write a function as set alert
-    const showAlert =(message,type)=>{
-      setAlert({
-        // we would aslo be able to specify the  msg as message. to avoid confusion i have stated here diff variable
-        msg:message,
-        // here both the type and type are different
-        type:type
-      })
-      // this time out will auto dissable the alert
-      setTimeout(() => {
-        // due to this after sometime the alert will be set to null. i,e it will be removed
-        setAlert(null)
-      }, 1000);
-
-    }
-
-
-
-
+  //state for alert
+  const [alert, setAlert] = useState(null);
+  // here we will be  defining alert/setAlert as an object and below i will write a function as set alert
+  const showAlert = (message, type) => {
+    setAlert({
+      // we would aslo be able to specify the  msg as message. to avoid confusion i have stated here diff variable
+      msg: message,
+      // here both the type and type are different
+      type: type,
+    });
+    // this time out will auto dissable the alert
+    setTimeout(() => {
+      // due to this after sometime the alert will be set to null. i,e it will be removed
+      setAlert(null);
+    }, 1000);
+  };
 
   // state for dark mode
   // dark mode enabled hain ya nahi main yeh yaha se control kar sakta.
-  const [mode, setMode] = useState('light') //whether dark mode is enabled or not.
+  const [mode, setMode] = useState("light"); //whether dark mode is enabled or not.
 
-  const togglemode = ()=>{
-    if(mode ==='light'){
-      setMode('dark')
+  const togglemode = () => {
+    if (mode === "light") {
+      setMode("dark");
       // here success type alert is shown which is green in color
-      showAlert("dark mode has been enabled","success")
-      
-      //now the problem arise that when we set the dark mode to on..we are not able to see the text form's text. so we have to declare it as a state varabel 
-      
-      
+      showAlert("dark mode has been enabled", "success");
+
+      //now the problem arise that when we set the dark mode to on..we are not able to see the text form's text. so we have to declare it as a state varabel
+
       // this also changes the color of the body. this line means that document mein(means webpage) body ka style(css) backgorundColor #042743 kardo
-      document.body.style.backgroundColor='#042743'
+      document.body.style.backgroundColor = "#042743";
 
       // when this funtions gets called/executed i.e when dark mode is enabled ...webpage title will be change
       // in this way we have accessed the css...document.title means doucment ka titile ye rakh diya
-      document.title="TextUtils -Dark Mode"
-
+      document.title = "TextUtils -Dark Mode";
 
       // this changes the title of the webchange frequently
       // setInterval(() => {
@@ -61,40 +55,52 @@ function App() {
       // }, 2000);
       // setInterval(() => {
       //   document.title="install text utils now"
-        
+
       // }, 750);
-    }
-    else{
-      setMode('light')
+    } else {
+      setMode("light");
       // this also changes the color of the body. this line means that document mein(means webpage) body ka style(css) backgorundColor white kardo
-      document.body.style.backgroundColor='white'
-      showAlert("light mode has been enabled","success")
-      document.title="TextUtils -Light Mode"
+      document.body.style.backgroundColor = "white";
+      showAlert("light mode has been enabled", "success");
+      document.title = "TextUtils -Light Mode";
     }
-  }
-
-
-
+  };
 
   return (
     <>
+      {/*  in <Router></Router> we write <Routes> </Routes> in this we write <Route path=" /about " element={<About/>}/> to go to About.js from the home page/App.js . also we need to Link the route path in this file(App,js) to the Navbar.js by writing <Link> <Link/> instead of <a></a> , and in <Link> <Link/> we have to add " to" instead of href. also keep in my that path in <Route/> and in "to" in <Link> <Link/> should be same to take affect. We could have also just use <a></a>  and href ,but it would reload the whole page and we dont want that*/}
 
-    {/* when something is passed default values wont be shown. when nothing is shown default value will be shown */}
-    {/* <Navbar title="Welcome" aboutText='loda lehsun'/> */}
-    {/* here specifymode and the toggle  are the funtions and their values are the props passed into the navbar */}
-    <Navbar title="Textutils"  specifymode={mode} toggle={togglemode}/>
-    
-      {/* here the state variable is passed in alert message. state variable is always passed in "{  }" */}
-      {/* here passing state variable alert is passed in Alert.js as prop which also means after the state change setAlert will also be passed. since setAlert is defined as object therefore meg and type will also be passed in the Alert.js*/}
-    <Alert alert_message={alert}/>
+      <Router>
+        {/* when something is passed default values wont be shown. when nothing is shown default value will be shown */}
+        {/* <Navbar title="Welcome" aboutText='loda lehsun'/> */}
+        {/* here specifymode and the toggle  are the funtions and their values are the props passed into the navbar */}
+        <Navbar title="Textutils" specifymode={mode} toggle={togglemode} />
 
-    <div className="container my-3">
-    <TextForm   alert_message={showAlert}  heading="Enter the text to analyze below: " specifymode={mode}/>
-    {/* <About/> */}
-    </div>
+        {/* here the state variable is passed in alert message. state variable is always passed in "{  }" */}
+        {/* here passing state variable alert is passed in Alert.js as prop which also means after the state change setAlert will also be passed. since setAlert is defined as object therefore meg and type will also be passed in the Alert.js*/}
+        <Alert alert_message={alert} />
 
+        <div className="container my-3">
+          <Routes>
+          {/* when we write the word exact react uses the exact matching of the path pr it uses the partial matching for example                                                                            consider the two paths /users-->Component1
+                                /users/home-->-->Component2
+          so here if dont write the exact path the react will let us to the component1 only and not to the component2 where we want to go. so sometime it becomes necessary to write the exact path */}
+            <Route exact path="/about" element={<About />} />
+
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  alert_message={showAlert}
+                  heading="Enter the text to analyze below: "
+                  specifymode={mode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
-    
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,59 +1,116 @@
-
 // rfc is below template. "react funtion based component". type rfc to bring it.
 
-import React from 'react'
+import React from "react";
 // impt keyword for importing prop types
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
     // use the {} for writing the JS, and used the backtiks(``) so that i can use it as the template like fill in the blanks.
     // also ${light} actslike a  varivale we can replace anything in it
-    <nav className={`navbar navbar-expand-lg  navbar-${props.specifymode} bg-${props.specifymode}`}>
+    <nav
+      className={`navbar navbar-expand-lg  navbar-${props.specifymode} bg-${props.specifymode}`}
+    >
       <div className="container-fluid">
         {/* this props.title passes our title in the  app.js when we include it in in Navbar component. it is written in "{ }" because it is the javascript and js is always written in curlly brackets */}
-        <a className="navbar-brand" href="/">{props.title}</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        {/* since by using the <a> href=" "</a>  and after clicking the props.title here textUtils the whole page is reloading so we will be changeing the <a></a> to <Link><Link/> so that whole page would not get reloaded and only the below componet would appear*/}
+        {/* <a className="navbar-brand" href="/">
+          {props.title}
+        </a> */}
+
+        
+        <Link className="navbar-brand" to="/">
+          {props.title}
+        </Link>
+
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              {/* We could have also just use <a></a>  and href ,but it would reload the whole page and we dont want that */}
+
+              {/* <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a> */}
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">{props.aboutText}</a>
+              {/* <a className="nav-link" href="/">
+                {props.aboutText}
+              </a> */}
+              {/* since we have specifically entered the /about in to in below <Link><Link/> and in <Route path=" /about"/>  in the App.js , it will get us to the Above.js component  */}
+              <Link className="nav-link" to="/about">
+                {props.aboutText}
+              </Link>
             </li>
-
           </ul>
-                                              {/* here text-light will give the white color t our text  */}
-                                              {/* used the terinary operator here to add some condition on the props.specifymode. the condition says that if the mode is light then do it dark and vice vers. it only affects the text color..from dark to light */}
-          <div className={`form-check form-switch text-${props.specifymode==='light'?'dark':'light'} mx-4`}>
-                                                {/* put htmlFor instead of for in JSX */}
-            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable dark mode</label>
-            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"
-            onClick={props.toggle} />
+          {/* here text-light will give the white color t our text  */}
+          {/* used the terinary operator here to add some condition on the props.specifymode. the condition says that if the mode is light then do it dark and vice vers. it only affects the text color..from dark to light */}
+          <div
+            className={`form-check form-switch text-${
+              props.specifymode === "light" ? "dark" : "light"
+            } mx-4`}
+          >
+            {/* put htmlFor instead of for in JSX */}
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              Enable dark mode
+            </label>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+              onClick={props.toggle}
+            />
           </div>
           <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" style={{backgroundColor:props.specifymode==='dark'?'grey':'white'}} />
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              style={{
+                backgroundColor:
+                  props.specifymode === "dark" ? "grey" : "white",
+              }}
+            />
             {/* success is for green color, primary is for blue color */}
-            <button className="btn btn-primary " type="submit" >Search</button>
+            <button className="btn btn-primary " type="submit">
+              Search
+            </button>
           </form>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
 // these are the proptypes it ssays that the title should be string or it will show error in the console.this we used to catch the error . it acts like a check
 
 Navbar.propTypes = {
-  title: PropTypes.string.isRequired,    //when written is requird and the default prop is not set here  then we have to pass the title in app.js or it will show error . ",isrequired mean which should state something there is should not be udefined "
-  aboutText: PropTypes.string
+  title: PropTypes.string.isRequired, //when written is requird and the default prop is not set here  then we have to pass the title in app.js or it will show error . ",isrequired mean which should state something there is should not be udefined "
+  aboutText: PropTypes.string,
 };
 
 // when prop are not passed these values will be executed
 Navbar.defaultProps = {
-  title: 'set title here',
-  aboutText: 'about'
+  title: "set title here",
+  aboutText: "about",
 };
